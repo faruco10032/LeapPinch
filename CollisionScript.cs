f7a2faa2d032c80bdce01116e08f846ca47223d3
Takayuki Kameoka <T.kameoka10032@gmail.com>
2019-10-31 19:09:10 +0900

    create repository

---
+++ CollisionScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    public float collider_radius = 0.02f;
+    public bool collision_flag = false;
+    public GameObject collision_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(collision_flag);
+    }
+
+    void OnTriggerStay(Collider t){//なにかに接触している間呼ばれ続ける．接触している物体をtとして格納
+        //接触している物体を取得
+        collision_object = t.gameObject;
+
+        //接触している物体の位置をベクトルで取得
+        Vector3 t_pos = t.transform.position;
+
+        // 接触している物体と自身の距離を計算
+        float distance = Vector3.Distance(this.transform.position, t_pos);
+        // Debug.Log("distance : "+ distance);
+
+        // 中心間距離に応じて接触判定を更新
+        if(distance<collider_radius){
+            collision_flag=true;
+        }else{
+            collision_flag=false;
+        }
+    }
+
+    void OnTriggerExit(){//接触判定が外れたら呼ばれる
+        collision_object = null;//物体から離れたらGameobjectをクリア
+    }
+}
---
+++ PinchScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchScript : MonoBehaviour
+{
+    public GameObject thumb;
+    public GameObject index;
+    CollisionScript thumb_script;
+    CollisionScript index_script;
+
+    // Start is called before the first frame update
+    void Start(){
+        //Gameobjectのスクリプトコンポーネントを取得
+        thumb_script = thumb.gameObject.GetComponent<CollisionScript>();
+        index_script = index.gameObject.GetComponent<CollisionScript>();
+        Debug.Log("Start!");
+    }
+
+    void Update(){
+        // 球の接触判定を取得
+        if(thumb_script.collision_flag&&index_script.collision_flag){//親指と人差し指両方で触れているとき
+            // Debug.Log("pinch now");
+            //指の球体コライダーの位置をベクトルで取得
+            Vector3 thumb_pos = thumb.transform.position;
+            Vector3 index_pos = index.transform.position;
+            //2点間の中心を計算
+            Vector3 centor_pos = (thumb_pos+index_pos)*0.5f;
+            //親指で接触してる物体の位置を2点間の中心へ移動
+            thumb_script.collision_object.transform.position = centor_pos;
+
+        }
+
+    }
+}   
