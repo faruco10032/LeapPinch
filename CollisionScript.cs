1d2ac9b12162d42362679e81e9f395c66b8b51fa
Takayuki Kameoka <T.kameoka10032@gmail.com>
2019-11-04 20:01:54 +0900

    接触物体がisEnablePinchを持ってるときだけ接触判定を入れる

--- CollisionScript.cs
+++ CollisionScript.cs
@@ -21,11 +21,14 @@ public class CollisionScript : MonoBehaviour
         // Debug.Log(collision_flag);
     }
 
-    void OnTriggerEnter(){// 接触した瞬間
-        collision_flag = true;
+    void OnTriggerEnter(Collider t){// 接触した瞬間
+        if(t.gameObject.tag == "isEnablePinch"){
+            collision_flag = true;
+        } 
     }
 
     void OnTriggerStay(Collider t){//なにかに接触している間呼ばれ続ける．接触している物体をtとして格納
+        if(collision_flag){
         //接触している物体を取得
         collision_object = t.gameObject;
 
@@ -38,15 +41,17 @@ public class CollisionScript : MonoBehaviour
 
         // 接触している物体のX軸を取得（R）を取得→のちほど大きさに変更する．rayを使って実装できないかな？
         R = t.transform.localScale.x*0.5f;
-
+        }
     }
 
     void OnTriggerExit(){//接触判定が外れたら呼ばれる
-        // 物体から離れたら重力を入れる
-        Rigidbody rb = collision_object.GetComponent<Rigidbody>();
-        rb.useGravity = true;
-        
-        collision_flag = false;// 物体から離れたらフラグを折る
-        collision_object = null;//物体から離れたらGameobjectをクリア
+        if(collision_flag){
+            // 物体から離れたら重力を入れる
+            Rigidbody rb = collision_object.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            
+            collision_flag = false;// 物体から離れたらフラグを折る
+            collision_object = null;//物体から離れたらGameobjectをクリア
+        }
     }
 }
